faef05be99c332a84ee030a319f79aaabee18ccc
akhookuminstitute <motahar.malik@ookla.com>
2026-09-25 09:29:20 +0100

    rogershdmobile 1.0.0 binary package

---
+++ Package.swift
@@ -0,0 +1,34 @@
+//
+//  Untitled.swift
+//  rogershdmobile
+//
+//  Created by motahar malik on 25/09/2026.
+//
+
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "rogershdmobile",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(
+            name: "rogershdmobile",
+            targets: ["rogershdmobile", "SpeedtestSDK"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "rogershdmobile",
+            url: "https://github.com/SpatialBuzz/hdmobile-ios-ca-spm-r/releases/download/1.0.0/rogershdmobile.xcframework.zip",
+            checksum: "eb769a20174cd3b32e1d1e4f3076faabfa0e7de30cd52fe1f1a7e60b2e3cd3cf"
+        ),
+        .binaryTarget(
+            name: "SpeedtestSDK",
+            url: "https://github.com/SpatialBuzz/hdmobile-ios-ca-spm-r/releases/download/1.0.0/SpeedtestSDK.xcframework.zip",
+            checksum: "23b2cfac4f2e9bb325ac929d9f3f0e7a60e826da03c1c32b9e2a007bc8eb9cda"
+        )
+    ]
+)
